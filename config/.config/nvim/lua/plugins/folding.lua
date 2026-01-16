43c7bc1876afe0b6b059d73d72e2dfe8fc16fd37
Harish Rajagopal <harish.rajagopals@gmail.com>
2026-01-16 23:34:21 +0100

    Pin nvim-ufo to fix fold previews
    Issue: https://github.com/kevinhwang91/nvim-ufo/issues/309

--- config/.config/nvim/lua/plugins/folding.lua
+++ config/.config/nvim/lua/plugins/folding.lua
@@ -13,6 +13,7 @@ end
 
 return {
 	"kevinhwang91/nvim-ufo",
+	commit = "5b75cf5fdb74054fc8badb2e7ca9911dc0470d94",
 	dependencies = "kevinhwang91/promise-async",
 	opts = {
 		provider_selector = function(bufnr, filetype, buftype)
